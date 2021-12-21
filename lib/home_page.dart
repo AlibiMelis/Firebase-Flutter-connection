5d1e4e10dadc96b9549b0bb25bb6f1c1fae8330d
Alibi Melis <melis.alibi2000@gmail.com>
2021-12-21 16:20:42 +0800

    Connected to Firebase

---
+++ lib/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Firebase Connection Check'),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('testing').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) return const SizedBox.shrink();
+          final data = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (BuildContext context, int index) {
+              final docData = data[index];
+              final dateTime = (docData['timestamp'] as Timestamp).toDate();
+              return ListTile(
+                title: Text(dateTime.toString()),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('testing')
+              .add({'timestamp': Timestamp.fromDate(DateTime.now())});
+        },
+      ),
+    );
+  }
+}
